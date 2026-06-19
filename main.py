7edb3556b7c5f004cc0590ae9e7cdf295f3b2f71
Roman Ch. <servermeteor@gmail.com>
2026-06-19 22:25:19 +0300

    big fix, better error handling, different id generating for tasks (simpler), marking tasks as on-going (unfinished)

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
 tasks = []
+next_id = 1
 
 
 def addTask() -> None:
-    global tasks
+    global next_id
 
     task = input("\nEnter a task: ")
 
@@ -13,29 +14,31 @@ def addTask() -> None:
         return
 
     task_data = {
-        "id": len(tasks) + 1,
+        "id": next_id,
         "content": task,
         "completed": False,
         "when_created": datetime.now(),
     }
 
-    tasks += [task_data]
+    tasks.append(task_data)
+    next_id += 1
+
     print("\nJust added a task!")
-    input("Press any key to conitnue...")
+    input("Press any key to continue...")
     print("\n" * 2)
 
 
 def listTasks() -> None:
     print("\nTasks\n")
 
-    i = 0
+    count = 0
     for task in tasks:
-        i += 1
+        count += 1
         print(
-            f"{i}. ID: {task["id"]}. Content: {task["content"]}. When created: {task["when_created"]}. Completed: {"✅" if task["completed"] else "❌"}"
+            f"{count}. ID: {task["id"]}. Content: {task["content"]}. When created: {task["when_created"]}. Completed: {"✅" if task["completed"] else "❌"}"
         )
 
-    if i == 0:
+    if count == 0:
         print("No tasks :(")
 
     print("\nFinished successfully!")
@@ -45,7 +48,7 @@ def listTasks() -> None:
 
 def sortTasks() -> None:
     sort_type = input(
-        "\nHow do you want to sort tasks?\n1. Newest\n2. Oldest\n3. Finished\n4. Unfinished\n>"
+        "\nHow do you want to sort tasks?\n1. Newest\n2. Oldest\n3. Completed\n4. On-going\n>"
     )
 
     for i in range(len(tasks) - 1):
@@ -55,27 +58,27 @@ def sortTasks() -> None:
             else:
                 match (sort_type):
                     case "1":
-                        if tasks[i]["when_created"] > tasks[j]["when_created"]:
-                            tasks[i], tasks[j] = tasks[j], tasks[i]
-                    case "2":
                         if tasks[i]["when_created"] < tasks[j]["when_created"]:
                             tasks[i], tasks[j] = tasks[j], tasks[i]
+                    case "2":
+                        if tasks[i]["when_created"] > tasks[j]["when_created"]:
+                            tasks[i], tasks[j] = tasks[j], tasks[i]
                     case "3":
-                        if not tasks[i]["finished"] and tasks[j]["finished"]:
+                        if not tasks[i]["completed"] and tasks[j]["completed"]:
                             tasks[i], tasks[j] = tasks[j], tasks[i]
                     case "4":
-                        if tasks[i]["finished"] and not tasks[j]["finished"]:
+                        if tasks[i]["completed"] and not tasks[j]["completed"]:
                             tasks[i], tasks[j] = tasks[j], tasks[i]
 
-    print("\nSortting is finished!")
+    print("\nSorting is finished!")
     input("Press any key to continue...")
     print("\n" * 2)
 
 
 def markTaskAsCompleted() -> None:
-    id = int(input("Enter task ID: "))
-
-    if not id:
+    try:
+        id = int(input("Enter task ID: "))
+    except ValueError:
         print("FAILURE: id must be a number.")
         input("Press any key to continue...")
         print("\n" * 2)
@@ -98,10 +101,36 @@ def markTaskAsCompleted() -> None:
     print("\n" * 2)
 
 
-def removeTaskById() -> None:
-    id = int(input("Enter ID: "))
+def markTaskAsOnGoing() -> None:
+    try:
+        id = int(input("Enter task ID: "))
+    except ValueError:
+        print("FAILURE: id must be a number.")
+        input("Press any key to continue...")
+        print("\n" * 2)
+        return
 
-    if not id:
+    changed = False
+    for task in tasks:
+        if task["id"] == id:
+            task["completed"] = False
+            changed = True
+            break
+
+    match changed:
+        case True:
+            print("Successfully marked the task as on-going!")
+        case False:
+            print("Failed to find a task.")
+
+    input("Press any key to continue...")
+    print("\n" * 2)
+
+
+def removeTaskById() -> None:
+    try:
+        id = int(input("Enter ID: "))
+    except ValueError:
         print("FAILURE: id must be a number.")
         input("Press any key to continue...")
         print("\n" * 2)
@@ -134,7 +163,7 @@ def main() -> str | None:
 
     print("Operations: ")
     choice = input(
-        "1. Add task\n2. List tasks\n3. Sort tasks\n4. Mark task as completed\n5. Remove task\n6. Exit\n> "
+        "1. Add task\n2. List tasks\n3. Sort tasks\n4. Mark task as completed\n5. Mark task as on-going\n6. Remove task\n7. Exit\n> "
     )
 
     match choice:
@@ -147,8 +176,10 @@ def main() -> str | None:
         case "4":
             markTaskAsCompleted()
         case "5":
-            removeTaskById()
+            markTaskAsOnGoing()
         case "6":
+            removeTaskById()
+        case "7":
             return "Cancelled"
 
 
